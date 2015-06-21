0c953507876e6668f1929d0b3c039ea243c94165
Kata <ladanyikata89@gmail.com>
2015-06-21 14:30:27 +0200

    project struktúra

--- Gitáros mise API/index.php
+++ Gitáros mise API/index.php
@@ -1,18 +1,20 @@
 <?php
 
+include_once 'rest.php';
+
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE');
 
 error_reporting(-1);
 
-$target = $_REQUEST['q'];
+$path = $_REQUEST['q'];
+$method = $_SERVER['REQUEST_METHOD'];
+$parameters = $_GET;
+unset($parameters['q']);
 $headers = getallheaders();
 $body = json_decode(file_get_contents('php://input'));
 
-$respose = array(
-    'path' => $target,
-    'body' => $body,
-    'headers' => $headers
-);
+$rest = new Rest();
 
-echo json_encode($respose, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+$rest->go($path, $method, $headers, $parameters, $body);
---
+++ Gitáros mise API/rest.php
@@ -0,0 +1,26 @@
+<?php
+
+class Rest {
+
+    public function go($path, $method, $headers, $parameters, $body) {
+        if (preg_match("/\/enekek\/([^?.]*)/i", $path, $matches)) {
+            include_once 'oldalak/enekek.php';
+            $mode = $matches[1];
+            echo "$method énekek, mód: $mode\n";
+        } else if (preg_match("/\/enek\/([^?.]*)/i", $path, $matches)) {
+            include_once 'oldalak/enekek.php';
+            $id = $matches[1];
+            echo "$method ének, id: $id\n";
+        } else {
+            $respose = array(
+                'path' => $target,
+                'method' => $method,
+                'parameters' => $parameters,
+                'body' => $body,
+                'headers' => $headers
+            );
+            echo json_encode($respose, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        }
+    }
+
+}
